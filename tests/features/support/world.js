2335eec8457dcdb32b4caf5a68d21918a8495a3e
Amitkumar Chauhan <amitkumar.chauhan@theoptimum.net>
2016-05-02 11:29:36 +0800

    changes done to pass the test case

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -1,8 +1,10 @@
-var World = function (callback) {
+var World = function(callback) {
 	// set up code goes here
 
 	// last line to tell cucumber.js the World is ready.
-	callback(this);
+	if (callback) {
+		callback(this);
+	}
 };
 
 exports.World = World;
